76ae305db4cbe80edaf9b9356a9edea29a9b9702
Leon Stoldt <leon.stoldt.a17b@nordakademie.org>
2019-12-03 00:07:07 +0100

    defined consuming and producing value of endpoint

--- src/main/java/de/cloud/fundamentals/urlshortenerservice/rest/Controller.java
+++ src/main/java/de/cloud/fundamentals/urlshortenerservice/rest/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
         return "UrlShortener is active.";
     }
 
-    @PostMapping("/api")
+    @PostMapping(value = "/api", produces = JSON, consumes = JSON)
     public ResponseEntity<String> receiveRequest(@RequestBody String message) {
         return ResponseEntity
                 .status(HttpStatus.OK)
